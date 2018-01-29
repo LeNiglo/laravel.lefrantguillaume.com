f7a80f22fe9b81ac65933dcd5650af3f4beb30ac
LeNiglo <lefrantguillaume@gmail.com>
2018-01-29 16:35:20 +0100

    WIP in ToolsController.php

--- app/Http/Controllers/ToolsController.php
+++ app/Http/Controllers/ToolsController.php
@@ -9,13 +9,12 @@ class ToolsController extends Controller
 	public function pictureProxying(Request $request)
 	{
 		if ($request->has('img')) {
-			$imginfo = @getimagesize($request->get('img'));
-			if ($imginfo) {
-				header("Content-type: " . $imginfo['mime']);
-				return readfile($request->get('img'));
-			} else {
-				abort(404);
+			$imginfo = getimagesize($request->img);
+			if (stripos($imginfo['mime'], 'image/') === false) {
+				abort(400);
 			}
+			header("Content-type: ".$imginfo['mime']);
+			return readfile($request->img);
 		} else {
 			abort(401);
 		}
